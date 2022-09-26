9592a942fa7f5bf6e05803e9a31808faab198125
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-09-26 16:26:50 +0900

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ export function Header() {
   return (
     <header class="text-lg border-t-2 border-black p-1">
       <a href="/" class="block">
-        <span class="text-2xl italic">[[Page Title]]</span>{" "}
+        <span class="text-2xl italic">カーシェア予約サイト</span>{" "}
         - カーシェアのサイトです。
       </a>
     </header>
